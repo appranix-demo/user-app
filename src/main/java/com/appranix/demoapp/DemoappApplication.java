35963203bd9393bda959ac2f5fe574dc7112f25c
Veeresh <veereshwaran@appranix.com>
2017-10-12 10:18:47 +0530

    Spring Context closed.

--- src/main/java/com/appranix/demoapp/DemoappApplication.java
+++ src/main/java/com/appranix/demoapp/DemoappApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -18,7 +19,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class DemoappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = null;
+        try {
+            context = SpringApplication.run(DemoappApplication.class, args);
+        } finally {
+            if (context != null) {
+                context.close();
+            }
+        }
+    }
 }
